7eefc5b15c6dbcb22e79a94fef17d7b033b640a3
Tuanldas <tuanldas@gmail.com>
2024-12-10 15:42:14 +0700

    Xóa content

--- src/layouts/demo1/index.ts
+++ src/layouts/demo1/index.ts
@@ -2,7 +2,6 @@ export * from './Demo1Layout';
 export * from './Demo1LayoutConfig';
 export * from './Demo1LayoutProvider';
 export * from './breadcrumbs';
-export * from './content';
 export * from './header';
 export * from './main';
 export * from './sidebar';
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,22 @@
+import { Fragment } from 'react';
+import { Container } from '@/components';
+import { Toolbar, ToolbarHeading, ToolbarPageTitle } from '@/partials/toolbar';
+
 const Dashboard = () => {
-  return <></>;
+  return (
+    <>
+      <Fragment>
+        <Container>
+          <Toolbar>
+            <ToolbarHeading>
+              <ToolbarPageTitle text={'NFT'} />
+            </ToolbarHeading>
+          </Toolbar>
+        </Container>
+        <Container>{/*<NetworkNFTContent />*/}</Container>
+      </Fragment>
+    </>
+  );
 };
 
 export default Dashboard;
