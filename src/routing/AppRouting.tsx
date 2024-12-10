f9a6cf76705d90291178c51f3f9828d7bc9bde67
Tuanldas <tuanldas@gmail.com>
2024-12-10 13:44:24 +0700

    Cập nhật page default

--- src/routing/AppRouting.tsx
+++ src/routing/AppRouting.tsx
@@ -1,7 +1,42 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { AppRoutingSetup } from '.';
+import { useAuthContext } from '@/auth';
+import { useLoaders } from '@/providers';
+import { useLocation } from 'react-router-dom';
 
 const AppRouting = (): ReactElement => {
+  const { setProgressBarLoader } = useLoaders();
+  const { setLoading } = useAuthContext();
+  const [previousLocation, setPreviousLocation] = useState('');
+  const [firstLoad, setFirstLoad] = useState(true);
+  const location = useLocation();
+  const path = location.pathname.trim();
+
+  useEffect(() => {
+    if (firstLoad) {
+      setLoading(false);
+      setFirstLoad(false);
+    }
+  }, [firstLoad, setLoading]);
+
+  useEffect(() => {
+    if (!firstLoad) {
+      setProgressBarLoader(true);
+      setPreviousLocation(path);
+      setProgressBarLoader(false);
+      if (path === previousLocation) {
+        setPreviousLocation('');
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  useEffect(() => {
+    if (!CSS.escape(window.location.hash)) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }, [previousLocation]);
+
   return <AppRoutingSetup />;
 };
 
--- src/routing/AppRoutingSetup.tsx
+++ src/routing/AppRoutingSetup.tsx
@@ -4,10 +4,16 @@ import { RequireAuth } from '@/auth';
 import { Demo1Layout } from '@/layouts/demo1';
 import { ErrorsRouting } from '@/errors';
 import { AuthPage } from '@/auth/AuthPage.tsx';
+import Dashboard from '@/pages/dashboard';
 
 const AppRoutingSetup = (): ReactElement => {
   return (
     <Routes>
+      <Route element={<RequireAuth />}>
+        <Route element={<Demo1Layout />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Route>
       <Route path="error/*" element={<ErrorsRouting />} />
       <Route path="auth/*" element={<AuthPage />} />
       <Route path="*" element={<Navigate to="/error/404" />} />
