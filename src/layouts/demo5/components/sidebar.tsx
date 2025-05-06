5fd14cd9498fb2055e0f5928d3b50272512f03b9
tuanldas <tuanldas@gmail.com>
2025-05-06 16:01:57 +0700

    Xử lý header

--- src/layouts/demo5/components/sidebar.tsx
+++ src/layouts/demo5/components/sidebar.tsx
@@ -1,13 +1,9 @@
-import { useLocation } from 'react-router-dom';
-import { SidebarMenuDashboard } from './sidebar-menu-dashboard';
-import { SidebarMenuDefault } from './sidebar-menu-default';
+import {SidebarMenuDashboard} from './sidebar-menu-dashboard';
 
 export function Sidebar() {
-  const { pathname } = useLocation();
-
-  return (
-    <div className="flex items-stretch shrink-0 px-2 w-(--sidebar-width) bg-background">
-      {pathname === '/' ? <SidebarMenuDashboard /> : <SidebarMenuDefault />}
-    </div>
-  );
+    return (
+        <div className="flex items-stretch shrink-0 px-2 w-(--sidebar-width) bg-background">
+            <SidebarMenuDashboard/>
+        </div>
+    );
 }
