b27d947d618d6bfd51f7677110b0a3f78e576322
tuanldas <tuanldas@gmail.com>
2025-05-25 16:14:15 +0700

    .

--- src/pages/Wallets/WalletContent.tsx
+++ src/pages/Wallets/WalletContent.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {useInfiniteQuery, useQuery} from '@tanstack/react-query';
+import {useEffect, useState} from 'react';
+import {useInfiniteQuery} from '@tanstack/react-query';
 import {callApiGetWallets} from '@/api/wallet';
 import {WalletRow} from './WalletRow';
 import {FormattedMessage} from 'react-intl';
@@ -26,7 +26,10 @@ interface ICampaignsContentItem {
     };
 }
 
+type ICampaignsContentItems = Array<ICampaignsContentItem>
+
 const WalletContent = () => {
+    const [items, setItems] = useState<ICampaignsContentItems>([]);
     const {data, isLoading, error, fetchNextPage, hasNextPage} = useInfiniteQuery({
         queryKey: ['wallets'],
         queryFn: callApiGetWallets,
@@ -35,15 +38,29 @@ const WalletContent = () => {
             if (data.current_page < data.last_page) {
                 return data.current_page + 1;
             }
-        },
-    });
-    const query = useQuery({
-        queryKey: ['testSimpleQuery'],
-        queryFn: async () => {
-            await new Promise(resolve => setTimeout(resolve, 100)); // Giả lập fetch
-            return { message: 'Test query working!' };
         }
     });
+
+    useEffect(() => {
+        if (!isLoading && data) {
+            let convertData = [...items];
+            data.pages.forEach((page) => {
+                const pageData = page.data;
+                const convertData2 = pageData.map((item: any) => {
+                    return {
+                        id: item.id,
+                        icon: item.icon,
+                        logoSize: '50px',
+                        title: item.name,
+                        description: renderCurrency(item.balance, item.currency),
+                        statistics: []
+                    };
+                });
+                convertData = [...convertData, ...convertData2];
+            });
+            setItems(convertData);
+        }
+    }, [isLoading, data]);
 
     useEffect(() => {
         if (error) {
@@ -80,30 +97,15 @@ const WalletContent = () => {
             </div>
         );
     }
-
-
-    if (isLoading) {
-        return <p>Đang tải bài viết...</p>;
-    }
-
     return (
         <div className="flex flex-col items-stretch gap-5 lg:gap-7.5">
             <div className="flex flex-wrap items-center gap-5 justify-between"></div>
 
             <div id="campaigns_list">
                 <div className="flex flex-col gap-5 lg:gap-7.5 pb-5 lg:pb-7.5">
-                    {data?.pages.map((page, pageIndex) => (
-                        <React.Fragment key={pageIndex}>
-                            {page.data.map((item) => renderItem({
-                                id: item.id,
-                                icon: item.icon,
-                                logoSize: '50px',
-                                title: item.name,
-                                description: renderCurrency(item.balance, item.currency),
-                                statistics: []
-                            }, item.id))}
-                        </React.Fragment>
-                    ))}
+                    {items.map((data, index) => {
+                        return renderItem(data, index);
+                    })}
                 </div>
                 {hasNextPage ? (
                     <div className="flex grow justify-center pt-5 lg:pt-7.5">
